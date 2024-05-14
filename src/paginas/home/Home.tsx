4b473d0305c1b2c17c47c868a9edb8104b1cf3be
darthlilac <dessalr@live.com>
2024-05-14 17:21:19 -0300

    Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Home.css';
+
+export const Home = () => {
+  return (
+    <>
+    <h1 className='titulo'> Home</h1>
+    </>
+  )
+}
+
+export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
